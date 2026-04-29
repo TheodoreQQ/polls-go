150b589fb6f6e5b030cc499dbe8e29209de9182d
Kamil Teodorowski <kamil.teo04@gmail.com>
2026-04-29 19:01:00 +0200

    feat: add new functions

--- internal/handlers/poll_handler.go
+++ internal/handlers/poll_handler.go
@@ -20,11 +20,21 @@ func (h *PollHandler) CreatePoll(c *gin.Context) {
 		return
 	}
 
-	query := `INSERT INTO polls (question) VALUES ($1) RETURNING id`
-	err := h.DB.QueryRow(query, p.Question).Scan(&p.ID)
+	queryPoll := `INSERT INTO polls (question) VALUES ($1) RETURNING id`
+	err := h.DB.QueryRow(queryPoll, p.Question).Scan(&p.ID)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Błąd zapisu do bazy"})
 		return
+	}
+
+	for i := range p.Options {
+		queryOption := `INSERT INTO options (poll_id, text) VALUES ($1, $2) RETURNING id`
+		err := h.DB.QueryRow(queryOption, p.ID, p.Options[i].Text).Scan(&p.Options[i].ID)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Błąd zapisu opcji"})
+			return
+		}
+		p.Options[i].PollID = p.ID
 	}
 
 	c.JSON(http.StatusCreated, p)
--- internal/models/poll.go
+++ internal/models/poll.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
 	ID       int    `json:"id"`
 	Username string `json:"username"`
-	Password string `json:"-"`
+	Password string `json:"password"`
 }
 
 type Poll struct {
