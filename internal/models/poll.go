3616550d506ca7d0c57bcb7fb66673d78d1078ec
Kamil Teodorowski <kamil.teo04@gmail.com>
2026-05-06 21:45:55 +0200

    refactor: transfer logic to other models to improve readability

--- internal/models/poll.go
+++ internal/models/poll.go
@@ -4,15 +4,16 @@ import "time"
 
 type User struct {
 	ID       int    `json:"id"`
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
 
 type Poll struct {
 	ID        int       `json:"id"`
-	Question  string    `json:"question"`
-	OwnerID   int       `json:"owner_id"`
-	Options   []Option  `json:"options"`
+	Question  string    `json:"question" binding:"required"`
+	OwnerID   int       `json:"-"`
+	Options   []Option  `json:"options" binding:"required"`
+	Code      string    `json:"code"`
 	IsActive  bool      `json:"is_active"`
 	CreatedAt time.Time `json:"created_at"`
 }
@@ -27,15 +28,4 @@ type Option struct {
 type Vote struct {
 	OptionID int `json:"option_id"`
 	VoterID  int `json:"voter_id"`
-}
-
-type PollForUser struct {
-	ID       int          `json:"id"`
-	Question string       `json:"question"`
-	Options  []OptionUser `json:"options"`
-}
-
-type OptionUser struct {
-	ID   int    `json:"id"`
-	Text string `json:"text"`
 }
