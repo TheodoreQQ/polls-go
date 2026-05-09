53f6387debcf62292f39345a14a3daa60f8261e0
Kamil Teodorowski <kamil.teo04@gmail.com>
2026-05-10 01:21:55 +0200

    refactor: db operations moved to repository

--- internal/handlers/poll_handler.go
+++ internal/handlers/poll_handler.go
@@ -1,27 +1,43 @@
 package handlers
 
 import (
-	"database/sql"
+	// "fmt"
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
+	"strconv"
+
+	// "time"
 
 	"github.com/TheodoreQQ/polls-go/internal/models"
+	"github.com/TheodoreQQ/polls-go/internal/repository"
 	"github.com/TheodoreQQ/polls-go/internal/utils"
+
 	"github.com/gin-gonic/gin"
 )
 
 type PollHandler struct {
-	DB *sql.DB
+	Repo *repository.PollRepository
 }
 
 func (h *PollHandler) CreatePoll(c *gin.Context) {
-	var p models.Poll
+	var p models.ReponseForUser
 
 	if err := c.ShouldBindJSON(&p); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"error":   "Validation error",
-			"message": "Question and options are required"})
+			"error": "Invalid JSON format"})
+		return
+	}
+
+	userID, err := utils.GetUserId(c)
+
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+
+	if p.Question == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Question cannot be empty"})
 		return
 	}
 
@@ -37,45 +53,9 @@ func (h *PollHandler) CreatePoll(c *gin.Context) {
 		}
 	}
 
-	userID, exists := c.Get("user_id")
-
-	if !exists {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
-		return
-	}
-
-	if id, ok := userID.(int); ok {
-		p.OwnerID = id
-	} else if idFloat, ok := userID.(float64); ok {
-		p.OwnerID = int(idFloat)
-	}
-
-	tx, err := h.DB.Begin()
+	err = h.Repo.Create(&p, userID)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Błąd rozpoczęcia transakcji"})
-		return
-	}
-	defer tx.Rollback()
-
-	queryPoll := `INSERT INTO polls (question, owner_id) VALUES ($1, $2) RETURNING id, created_at`
-	err = tx.QueryRow(queryPoll, p.Question, p.OwnerID).Scan(&p.ID, &p.CreatedAt)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Błąd zapisu do bazy"})
-		return
-	}
-
-	for i := range p.Options {
-		queryOption := `INSERT INTO options (poll_id, text) VALUES ($1, $2) RETURNING id`
-		err := tx.QueryRow(queryOption, p.ID, p.Options[i].Text).Scan(&p.Options[i].ID)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Błąd zapisu opcji"})
-			return
-		}
-		p.Options[i].PollID = p.ID
-	}
-
-	if err = tx.Commit(); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Błąd zatwierdzania transakcji"})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Database error"})
 		return
 	}
 
@@ -84,126 +64,39 @@ func (h *PollHandler) CreatePoll(c *gin.Context) {
 
 func (h *PollHandler) GetPoll(c *gin.Context) {
 
-	var pCreatedAt time.Time
-	val, exists := c.Get("user_id")
-	if !exists {
+	userID, err := utils.GetUserId(c)
+	if err != nil {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
 		return
 	}
 
-	var userID int
-	switch v := val.(type) {
-	case int:
-		userID = v
-	case float64:
-		userID = int(v)
-	default:
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Błędny format ID użytkownika"})
-		return
-	}
-
-	queryPoll := `SELECT p.id, p.question, p.is_active, p.created_at,COALESCE(o.id, 0), COALESCE(o.text, ''), 
-	COALESCE(o.votes_count, 0)
-								FROM polls p 
-								LEFT JOIN options o ON p.id = o.poll_id
-								WHERE p.owner_id = $1
-								ORDER BY p.id`
-
-	rows, err := h.DB.Query(queryPoll, userID)
+	polls, err := h.Repo.GetPollById(userID)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Błąd pobierania"})
-		return
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch polls"})
 	}
 
-	defer rows.Close()
-
-	pollsMap := make(map[int]*models.Poll)
-
-	var pollsOrder []int
-
-	for rows.Next() {
-		var pID, oID, oVotes int
-		var pQuestion, oText string
-		var pActive bool
-
-		err := rows.Scan(&pID, &pQuestion, &pActive, &pCreatedAt, &oID, &oText, &oVotes)
-		if err != nil {
-			continue
-		}
-
-		if _, exists := pollsMap[pID]; !exists {
-			pollsMap[pID] = &models.Poll{
-				ID:        pID,
-				Question:  pQuestion,
-				IsActive:  pActive,
-				CreatedAt: pCreatedAt,
-				Options:   []models.Option{},
-			}
-			pollsOrder = append(pollsOrder, pID)
-		}
-		if oID > 0 {
-			pollsMap[pID].Options = append(pollsMap[pID].Options, models.Option{
-				ID:     oID,
-				PollID: pID,
-				Text:   oText,
-				Votes:  oVotes,
-			})
-
-		}
-	}
-
-	result := []models.Poll{}
-	for _, pollID := range pollsOrder {
-		result = append(result, *pollsMap[pollID])
-	}
-	c.JSON(http.StatusOK, result)
+	c.JSON(http.StatusOK, polls)
 }
 
 func (h *PollHandler) ActivatePoll(c *gin.Context) {
-	pollID := c.Param("id")
-	roomCode := utils.GenerateCode()
-
-	val, _ := c.Get("user_id")
-	var userID int
-
-	if id, ok := val.(int); ok {
-		userID = id
-	} else {
-		userID = int(val.(float64))
-	}
-
-	tx, err := h.DB.Begin()
+	strPollID := c.Param("id")
+	pollID, err := strconv.Atoi(strPollID)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Transaction failed"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid poll ID"})
 		return
 	}
 
-	defer tx.Rollback()
-
-	_, err = tx.Exec(`UPDATE polls SET is_active = false, code = NULL WHERE owner_id = $1`, userID)
+	userID, err := utils.GetUserId(c)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to reset polls"})
-		return
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
 	}
 
-	result, err := tx.Exec(`UPDATE polls SET is_active = true, code = $1 WHERE id = $2 AND owner_id = $3`, roomCode, pollID, userID)
+	roomCode, err := h.Repo.Activate(pollID, userID)
 
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update status"})
+		c.JSON(http.StatusNotFound, gin.H{"error": "Poll not found or you don't have permission"})
 		return
 	}
-
-	rowsAffected, _ := result.RowsAffected()
-	if rowsAffected == 0 {
-		c.JSON(http.StatusNotFound, gin.H{"error": "poll not found or no permission"})
-		return
-	}
-
-	if err := tx.Commit(); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save the data"})
-		return
-	}
-
 	c.JSON(http.StatusOK, gin.H{
 		"message": "poll activated succesfully",
 		"code":    roomCode})
@@ -212,47 +105,13 @@ func (h *PollHandler) ActivatePoll(c *gin.Context) {
 func (h *PollHandler) GetPollByCode(c *gin.Context) {
 	code := c.Param("code")
 
-	query := `SELECT p.id, p.question, o.id, o.text 
-						FROM polls p 
-						JOIN options o ON p.id = o.poll_id 
-						WHERE p.code = $1 AND p.is_active = true`
-
-	rows, err := h.DB.Query(query, code)
+	poll, err := h.Repo.GetPollByCode(code)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve data"})
+		c.JSON(http.StatusNotFound, gin.H{"error": "Poll not found"})
 		return
 	}
-	defer rows.Close()
 
-	var poll models.ResponseForStudent
-	first := true
-
-	for rows.Next() {
-		var pID, oID int
-		var pQuestion, oText string
-
-		if err := rows.Scan(&pID, &pQuestion, &oID, &oText); err != nil {
-			continue
-		}
-
-		if first {
-			poll.ID = pID
-			poll.Question = pQuestion
-			poll.Options = []models.OptionsForStudent{}
-			first = false
-		}
-
-		poll.Options = append(poll.Options, models.OptionsForStudent{
-			ID:   oID,
-			Text: oText,
-		})
-	}
-	if first {
-		c.JSON(http.StatusNotFound, gin.H{"error": "No active poll for that code"})
-		return
-	}
 	c.JSON(http.StatusOK, poll)
-
 }
 
 func (h *PollHandler) Vote(c *gin.Context) {
@@ -262,43 +121,25 @@ func (h *PollHandler) Vote(c *gin.Context) {
 		return
 	}
 
-	var pollID int
-	var isActive bool
-
-	err := h.DB.QueryRow("SELECT p.id, p.is_active FROM polls p JOIN options o ON p.id = o.poll_id WHERE o.id = $1", vote.OptionID).Scan(&pollID, &isActive)
-
+	pollID, err := h.Repo.GetPollIDByOption(vote.OptionID)
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Option not found"})
 		return
 	}
 
-	if !isActive {
-		c.JSON(http.StatusForbidden, gin.H{"error": "Poll is not active"})
-		return
-	}
 	cookieName := fmt.Sprintf("voted_poll_%d", pollID)
 	if _, err := c.Cookie(cookieName); err == nil {
 		c.JSON(http.StatusForbidden, gin.H{"error": "You have already voted!"})
 		return
 	}
 
-	tx, err := h.DB.Begin()
+	actualPollID, err := h.Repo.VotePoll(vote.OptionID)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Transaction failed"})
-		return
-	}
-	defer tx.Rollback()
-
-	query := `UPDATE options SET votes_count = votes_count + 1 WHERE id = $1 AND poll_id IN (SELECT id FROM polls WHERE is_active = true) 
-	RETURNING poll_id`
-
-	err = tx.QueryRow(query, vote.OptionID).Scan(&pollID)
-	if err != nil {
-		c.JSON(http.StatusForbidden, gin.H{"error": "Failed to update votes"})
+		c.JSON(http.StatusForbidden, gin.H{"error": "Poll is deactivated or you chose wrong option"})
 		return
 	}
 
-	cookieName = fmt.Sprintf("voted_poll_%d", pollID)
+	cookieName = fmt.Sprintf("voted_poll_%d", actualPollID)
 	c.SetCookie(
 		cookieName,
 		"true",
@@ -309,148 +150,73 @@ func (h *PollHandler) Vote(c *gin.Context) {
 		true,
 	)
 
-	if err := tx.Commit(); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save the data"})
-		return
-	}
-
 	c.JSON(http.StatusOK, gin.H{"message": "Vote successful"})
 }
 
 func (h *PollHandler) GetVotesByPoll(c *gin.Context) {
-	pollID := c.Param("id")
+	strPollID := c.Param("id")
+	pollID, err := strconv.Atoi(strPollID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid poll ID"})
+		return
+	}
 
-	val, exists := c.Get("user_id")
-	if !exists {
+	userID, err := utils.GetUserId(c)
+	if err != nil {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
 		return
 	}
 
-	var userID int
-	switch v := val.(type) {
-	case int:
-		userID = v
-	case float64:
-		userID = int(v)
-	default:
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Błędny format ID użytkownika"})
-		return
-	}
-
-	query := `SELECT p.id, p.question, o.id, o.text, o.votes_count FROM polls p JOIN options o ON p.id = o.poll_id WHERE p.id = $1 AND p.owner_id = $2`
-
-	rows, err := h.DB.Query(query, pollID, userID)
+	response, err := h.Repo.GetVotesByPolll(pollID, userID)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve data"})
+		c.JSON(http.StatusNotFound, gin.H{"error": "Poll not found or you don't have permission"})
 		return
 	}
 
-	defer rows.Close()
-
-	var response models.PollResultsResponse
-	var totalVotes int
-
-	for rows.Next() {
-		var pID, oID, oVotes int
-		var pQuestion, oText string
-
-		err = rows.Scan(&pID, &pQuestion, &oID, &oText, &oVotes)
-		if err != nil {
-			continue
-		}
-
-		if response.ID == 0 {
-			response.ID = pID
-			response.Question = pQuestion
-		}
-
-		totalVotes += oVotes
-
-		response.Options = append(response.Options, models.OptionResult{
-			ID:         oID,
-			Text:       oText,
-			VotesCount: oVotes,
-		})
-	}
-
-	if response.ID == 0 {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Poll not found or no permission"})
-		return
-	}
-
-	response.TotalVotes = totalVotes
 	c.JSON(http.StatusOK, response)
 }
 
 func (h *PollHandler) DeletePoll(c *gin.Context) {
-	pollID := c.Param("id")
-	val, exists := c.Get("user_id")
+	strPollID := c.Param("id")
+	pollID, err := strconv.Atoi(strPollID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid poll ID"})
+		return
+	}
 
-	if !exists {
+	userID, err := utils.GetUserId(c)
+
+	if err != nil {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
 		return
 	}
 
-	var userID int
-
-	switch v := val.(type) {
-	case int:
-		userID = v
-	case float64:
-		userID = int(v)
-	default:
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "incorrect id format"})
-		return
-	}
-
-	query := `DELETE FROM polls WHERE id = $1 AND owner_id = $2`
-
-	result, err := h.DB.Exec(query, pollID, userID)
+	err = h.Repo.DeletePoll(pollID, userID)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete poll"})
-		return
-	}
-
-	rowsAffected, _ := result.RowsAffected()
-	if rowsAffected == 0 {
-		c.JSON(http.StatusNotFound, gin.H{"error": "poll not found or no permission"})
-		return
+		c.JSON(http.StatusNotFound, gin.H{"error": "Poll not found or no permission"})
 	}
 
 	c.JSON(http.StatusNoContent, gin.H{"message": "Poll deleted successfully"})
 }
 
 func (h *PollHandler) DeactivatePoll(c *gin.Context) {
-	pollID := c.Param("id")
-	val, exists := c.Get("user_id")
+	strPollID := c.Param("id")
+	pollID, err := strconv.Atoi(strPollID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid poll ID"})
+		return
+	}
 
-	if !exists {
+	userID, err := utils.GetUserId(c)
+
+	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve data"})
 		return
 	}
 
-	var userID int
-
-	switch v := val.(type) {
-	case int:
-		userID = v
-	case float64:
-		userID = int(v)
-	default:
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "incorrect id format"})
-		return
-	}
-
-	query := `UPDATE polls SET is_active = false WHERE is_active = true AND id = $1 AND owner_id = $2`
-
-	result, err := h.DB.Exec(query, pollID, userID)
+	err = h.Repo.DeactivatePoll(pollID, userID)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to deactivate poll"})
-		return
-	}
-	rowsAffected, _ := result.RowsAffected()
-	if rowsAffected == 0 {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Poll is already deactivated or does not exist or you don't have permission to deactivate it"})
+		c.JSON(http.StatusNotFound, gin.H{"error": "poll not found, no permission or is already deactivated"})
 		return
 	}
 
@@ -459,77 +225,39 @@ func (h *PollHandler) DeactivatePoll(c *gin.Context) {
 }
 
 func (h *PollHandler) UpdateQuestion(c *gin.Context) {
-	pollID := c.Param("id")
-	val, exists := c.Get("user_id")
-	var p models.UpdatePollWithOptionRequest
+	strPollID := c.Param("id")
+	pollID, err := strconv.Atoi(strPollID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid poll ID"})
+		return
+	}
 
-	if !exists {
+	userID, err := utils.GetUserId(c)
+
+	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve data"})
 		return
 	}
 
-	var userID int
-
-	switch v := val.(type) {
-	case int:
-		userID = v
-	case float64:
-		userID = int(v)
-	default:
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "incorrect id format"})
+	var req models.UpdatePollWithOptionRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid body"})
 		return
 	}
 
-	if err := c.ShouldBindJSON(&p); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+	if req.Question == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Question is empty"})
 		return
 	}
 
-	if p.Question == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Question cannot be empty"})
-		return
-	}
-
-	tx, err := h.DB.Begin()
+	err = h.Repo.UpdateQuestion(pollID, userID, req)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to start transaction"})
-		return
-	}
-
-	defer tx.Rollback()
-
-	query := `UPDATE polls SET question = $1 WHERE id = $2 AND owner_id = $3`
-
-	result, err := tx.Exec(query, p.Question, pollID, userID)
-
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update question"})
-		return
-	}
-
-	rowsAffected, _ := result.RowsAffected()
-	if rowsAffected == 0 {
-		c.JSON(http.StatusNotFound, gin.H{
-			"error": "Poll not found or no permission"})
-		return
-	}
-	for i := range p.Options {
-		queryOpt := `UPDATE options SET text = $1 WHERE id = $2 AND poll_id = $3`
-		result, err := tx.Exec(queryOpt, p.Options[i].Text, p.Options[i].ID, pollID)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save options"})
+		if errors.Is(err, repository.ErrNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Poll not found or no permission"})
 			return
 		}
-		if r, _ := result.RowsAffected(); r == 0 {
-			c.JSON(http.StatusNotFound, gin.H{"error": "option not found"})
-			return
-		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal error"})
+		return
 	}
-
-	if err := tx.Commit(); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to commit transaction"})
-	}
-
-	c.JSON(http.StatusOK, gin.H{"message": "question and/or options updated succesfully"})
-
+	c.JSON(http.StatusOK, gin.H{"message": "Updated successfully"})
 }
