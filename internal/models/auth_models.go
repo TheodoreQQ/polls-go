4a403421902706468b74c0059968cb8f729976a8
Kamil Teodorowski <kamil.teo04@gmail.com>
2026-05-15 21:44:03 +0200

    refactor: small changes in auth requests

--- internal/models/auth_models.go
+++ internal/models/auth_models.go
@@ -1,6 +1,11 @@
 package models
 
 type RegisterRequest struct {
-	Username string `json:"username" binding:"required,min=3"`
-	Password string `json:"password" binding:"required,min=3"`
+	Username string `json:"username" binding:"required,min=3" example:"TheodoreQQ"`
+	Password string `json:"password" binding:"required,min=3" example:"123"`
+}
+
+type LoginRequest struct {
+	Username string `json:"username" binding:"required" example:"TheodoreQQ"`
+	Password string `json:"password" binding:"required" example:"123"`
 }
